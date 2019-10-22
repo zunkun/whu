1111bf7e57c97648373fe331e07ec4fc4ff78c44
zunkun <liuzunkun@gmail.com>
2019-10-22 09:07:47 +0800

    配置

--- config/production.js
+++ config/production.js
@@ -1,14 +1,14 @@
 module.exports = {
 	PORT: 3000,
 	mysql: {
-		host: '127.0.0.1',
+		host: '10.113.4.75',
 		port: 3306,
 		database: 'whu',
 		username: 'root',
 		password: '!Asbcd1234'
 	},
 	postgres: {
-		host: '127.0.0.1',
+		host: '10.113.4.75',
 		port: 5432,
 		database: 'whu',
 		username: 'whu',
@@ -17,11 +17,11 @@ module.exports = {
 	secret: 'whu',
 	baseDeptId: 1,
 	dingBaseUri: 'https://oapi.dingtalk.com',
-	corpId: 'dingcbcbb63d3edd5478',
-	corpName: '上海铭悦软件有限公司',
-	agentId: '302567497',
-	appkey: 'dingi36m7pcjlcqapbci',
-	appsecret: 'rL9MrWWxdIaFquO-LtMKHsg_kAD8eMggbRAufVbCPCjtu-5iyE8RKi3C5nMln-lq',
+	corpId: 'ding2f9d4c2c3863312935c2f4657eb6378f',
+	corpName: '武汉大学校友总会',
+	agentId: '306193209',
+	appkey: 'dingtxfdh35depfohu0a',
+	appsecret: 'GLcPYrJIKkaFfCOdvyVMWNFevu1PiqdqRgtgFIZw5ZJqAXbtrYDIR_uOl0UoNQyn',
 	nonceStr: 'afasdzwe',
 	deptCron: '0 0 6 * * *', // 每日6时同步部门列表
 	roleCron: '0 0 */1 * *', // 每隔1个小时同步角色列表
